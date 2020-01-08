f6a24769262e5b3d573f4eead49e401390b57bb0
Unmesh Chowdhury <unmeshchow@gmail.com>
2020-01-08 21:18:08 +0600

    Updates

---
+++ precache-manifest.e380e490b2d9708cc4da83dec1aae498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8438c338722128e9234a706694544c80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c062458b42862ff36183",
+    "url": "/monsters-rolodex/static/css/main.b0d1b25f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c062458b42862ff36183",
+    "url": "/monsters-rolodex/static/js/main.5c89d954.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
